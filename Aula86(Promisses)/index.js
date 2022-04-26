f43174799eb983a7729ea7edfc929e6db15c2e52
Leandro Cunha <leandrolespaul01@hotmail.com>
2022-04-25 22:36:45 -0300

    Feito aula 86

---
+++ Aula86(Promisses)/index.js
@@ -0,0 +1,34 @@
+function rand(min, max) { // função para um tempo aleatório
+    min *= 1000
+    max *= 1000
+    return Math.floor(Math.random() * (max - min ) + min)
+}
+
+function esperaAi (msg, tempo) {
+    return new Promise((resolve, reject) => {
+        if(msg !== 'string') reject('ERROR')
+    setTimeout( () => {
+        resolve(msg)
+    }, tempo )
+    })
+}
+
+esperaAi('Conexão com DB', rand(1, 3))
+.then(resposta => {
+    console.log(resposta)
+    return esperaAi('Buscando dados da base', rand(1, 3))
+})
+.then(resposta => {
+    console.log(resposta)
+    return esperaAi(222222, rand(1, 3))
+})
+.then(resposta => {
+    console.log(resposta)
+}).then(() => {
+    console.log('Exibe os dados da tela')
+})
+.catch(error => {
+    console.log('Error', error)
+})
+
+console.log('Isso aqui será antes de qualquer promisse.')
