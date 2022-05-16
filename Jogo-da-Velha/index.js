6c8c014718937ed91a408b8cac0c9330173d4e09
Leandro Cunha <leandrolespaul01@hotmail.com>
2022-05-16 00:58:31 -0300

    Jogo da Velha fazendo

--- Jogo-da-Velha/index.js
+++ Jogo-da-Velha/index.js
@@ -9,19 +9,82 @@ let vezJogador = jogador1
 function jogar(casa) {
 
     if (typeof casa !== 'number' || casa > 8 || casa < 0) throw Error('Jogada inválida!')
-    
-    
+
+    if (typeof tabuleiro[casa] === 'string') throw Error('Casa já marcada. Escolha outra casa!')
+    // pensei no lance dos dados os tipos...
 
     tabuleiro[casa] = vezJogador
 
     vezJogador = vezJogador === jogador1 ? jogador2 : jogador1
 
+    if (tabuleiro[0] === jogador1 && tabuleiro[1] === jogador1 && tabuleiro[2] === jogador1) {
+        return console.log('Jogador 1 ganhou! Parabéns')
+    }
+    if (tabuleiro[3] === jogador1 && tabuleiro[4] === jogador1 && tabuleiro[5] === jogador1) {
+        return console.log('Jogador 1 ganhou! Parabéns')
+    }
+    if (tabuleiro[6] === jogador1 && tabuleiro[7] === jogador1 && tabuleiro[8] === jogador1) {
+        return console.log('Jogador 1 ganhou! Parabéns')
+    }
+    if (tabuleiro[0] === jogador1 && tabuleiro[4] === jogador1 && tabuleiro[8] === jogador1) {
+        return console.log('Jogador 1 ganhou! Parabéns')
+    }
+    if (tabuleiro[0] === jogador1 && tabuleiro[4] === jogador1 && tabuleiro[8] === jogador1) {
+        return console.log('Jogador 1 ganhou! Parabéns')
+    }
+    if (tabuleiro[0] === jogador1 && tabuleiro[3] === jogador1 && tabuleiro[6] === jogador1) {
+        return console.log('Jogador 1 ganhou! Parabéns')
+    }
+    if (tabuleiro[1] === jogador1 && tabuleiro[4] === jogador1 && tabuleiro[7] === jogador1) {
+        return console.log('Jogador 1 ganhou! Parabéns')
+    }
+    if (tabuleiro[2] === jogador1 && tabuleiro[5] === jogador1 && tabuleiro[8] === jogador1) {
+        return console.log('Jogador 1 ganhou! Parabéns')
+    }
 
+
+    if (tabuleiro[0] === jogador2 && tabuleiro[1] === jogador2 && tabuleiro[2] === jogador2) {
+        return console.log('Jogador 2 ganhou! Parabéns')
+    }
+    if (tabuleiro[3] === jogador2 && tabuleiro[4] === jogador2 && tabuleiro[5] === jogador2) {
+        return console.log('Jogador 2 ganhou! Parabéns')
+    }
+    if (tabuleiro[6] === jogador2 && tabuleiro[7] === jogador2 && tabuleiro[8] === jogador2) {
+        return console.log('Jogador 2 ganhou! Parabéns')
+    }
+    if (tabuleiro[0] === jogador2 && tabuleiro[4] === jogador2 && tabuleiro[8] === jogador2) {
+        return console.log('Jogador 2 ganhou! Parabéns')
+    }
+    if (tabuleiro[0] === jogador2 && tabuleiro[3] === jogador2 && tabuleiro[6] === jogador2) {
+        return console.log('Jogador 2 ganhou! Parabéns')
+    }
+    if (tabuleiro[1] === jogador2 && tabuleiro[4] === jogador2 && tabuleiro[7] === jogador2) {
+        return console.log('Jogador 2 ganhou! Parabéns')
+    }
+    if (tabuleiro[2] === jogador2 && tabuleiro[5] === jogador2 && tabuleiro[8] === jogador2) {
+        return console.log('Jogador 2 ganhou! Parabéns')
+    }
+    if (tabuleiro[2] === jogador2 && tabuleiro[4] === jogador2 && tabuleiro[6] === jogador2) {
+        return console.log('Jogador 2 ganhou! Parabéns')
+    }
+    
+    if (tabuleiro.map((valor) => typeof valor === 'string')) {
+        console.log('VELHA!')
+    }
 }
 
 
+jogar(0)
+jogar(1)
+jogar(2)
+jogar(6)
+jogar(7)
+jogar(5)
 jogar(4)
-jogar(4)
+jogar(3)
+jogar(8)
+
+
 
 
 
