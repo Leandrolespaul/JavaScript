2555e45072dfdcabfd1b961c4538d18b9d517870
Leandro Cunha <leandrolespaul01@hotmail.com>
2022-05-28 00:02:00 -0300

    Diminuimos o if

--- Jogo-da-Velha/index.js
+++ Jogo-da-Velha/index.js
@@ -9,6 +9,18 @@ let jogadores = [jogador1, jogador2]
 
 let vezJogador = rand(jogadores)
 
+const jogadasVencedoras = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+]
+
+
 console.log('Vez do Jogador: ', vezJogador)
 
 
@@ -23,26 +35,30 @@ function jogar(casa) {
 
     vezJogador = vezJogador === jogador1 ? jogador2 : jogador1
 
+    if (jogadasVencedoras.some(jogada => jogada.every(casa => tabuleiro[casa] === jogador1))) return jogadorVencedor(jogador1)
+
+    if (jogadasVencedoras.some(jogada => jogada.every(casa => tabuleiro[casa] === jogador2))) return jogadorVencedor(jogador2)
 
 
 
-    if (tabuleiro[0] === jogador1 && tabuleiro[1] === jogador1 && tabuleiro[2] === jogador1) return console.log('Jogador 1 ganhou! Parabéns!!!')
-    if (tabuleiro[3] === jogador1 && tabuleiro[4] === jogador1 && tabuleiro[5] === jogador1) return console.log('Jogador 1 ganhou! Parabéns!!!')
-    if (tabuleiro[6] === jogador1 && tabuleiro[7] === jogador1 && tabuleiro[8] === jogador1) return console.log('Jogador 1 ganhou! Parabéns!!!')
-    if (tabuleiro[0] === jogador1 && tabuleiro[4] === jogador1 && tabuleiro[8] === jogador1) return console.log('Jogador 1 ganhou! Parabéns!!!')
-    if (tabuleiro[2] === jogador1 && tabuleiro[4] === jogador1 && tabuleiro[6] === jogador1) return console.log('Jogador 1 ganhou! Parabéns!!!')
-    if (tabuleiro[0] === jogador1 && tabuleiro[3] === jogador1 && tabuleiro[6] === jogador1) return console.log('Jogador 1 ganhou! Parabéns!!!')
-    if (tabuleiro[1] === jogador1 && tabuleiro[4] === jogador1 && tabuleiro[7] === jogador1) return console.log('Jogador 1 ganhou! Parabéns!!!')
-    if (tabuleiro[2] === jogador1 && tabuleiro[5] === jogador1 && tabuleiro[8] === jogador1) return console.log('Jogador 1 ganhou! Parabéns!!!')
 
-    if (tabuleiro[0] === jogador2 && tabuleiro[1] === jogador2 && tabuleiro[2] === jogador2) return console.log('Jogador 2 ganhou! Parabéns!!!')
-    if (tabuleiro[3] === jogador2 && tabuleiro[4] === jogador2 && tabuleiro[5] === jogador2) return console.log('Jogador 2 ganhou! Parabéns!!!')
-    if (tabuleiro[6] === jogador2 && tabuleiro[7] === jogador2 && tabuleiro[8] === jogador2) return console.log('Jogador 2 ganhou! Parabéns!!!')
-    if (tabuleiro[0] === jogador2 && tabuleiro[4] === jogador2 && tabuleiro[8] === jogador2) return console.log('Jogador 2 ganhou! Parabéns!!!')
-    if (tabuleiro[2] === jogador2 && tabuleiro[4] === jogador2 && tabuleiro[6] === jogador2) return console.log('Jogador 2 ganhou! Parabéns!!!')
-    if (tabuleiro[0] === jogador2 && tabuleiro[3] === jogador2 && tabuleiro[6] === jogador2) return console.log('Jogador 2 ganhou! Parabéns!!!')
-    if (tabuleiro[1] === jogador2 && tabuleiro[4] === jogador2 && tabuleiro[7] === jogador2) return console.log('Jogador 2 ganhou! Parabéns!!!')
-    if (tabuleiro[2] === jogador2 && tabuleiro[5] === jogador2 && tabuleiro[8] === jogador2) return console.log('Jogador 2 ganhou! Parabéns!!!')
+    // if (tabuleiro[0] === jogador1 && tabuleiro[1] === jogador1 && tabuleiro[2] === jogador1) return jogadorXVenceu()
+    // if (tabuleiro[3] === jogador1 && tabuleiro[4] === jogador1 && tabuleiro[5] === jogador1) return jogadorXVenceu()
+    // if (tabuleiro[6] === jogador1 && tabuleiro[7] === jogador1 && tabuleiro[8] === jogador1) return jogadorXVenceu()
+    // if (tabuleiro[0] === jogador1 && tabuleiro[4] === jogador1 && tabuleiro[8] === jogador1) return jogadorXVenceu()
+    // if (tabuleiro[2] === jogador1 && tabuleiro[4] === jogador1 && tabuleiro[6] === jogador1) return jogadorXVenceu()
+    // if (tabuleiro[0] === jogador1 && tabuleiro[3] === jogador1 && tabuleiro[6] === jogador1) return jogadorXVenceu()
+    // if (tabuleiro[1] === jogador1 && tabuleiro[4] === jogador1 && tabuleiro[7] === jogador1) return jogadorXVenceu()
+    // if (tabuleiro[2] === jogador1 && tabuleiro[5] === jogador1 && tabuleiro[8] === jogador1) return jogadorXVenceu()
+
+    // if (tabuleiro[0] === jogador2 && tabuleiro[1] === jogador2 && tabuleiro[2] === jogador2) return jogadorOVenceu()
+    // if (tabuleiro[3] === jogador2 && tabuleiro[4] === jogador2 && tabuleiro[5] === jogador2) return jogadorOVenceu()
+    // if (tabuleiro[6] === jogador2 && tabuleiro[7] === jogador2 && tabuleiro[8] === jogador2) return jogadorOVenceu()
+    // if (tabuleiro[0] === jogador2 && tabuleiro[4] === jogador2 && tabuleiro[8] === jogador2) return jogadorOVenceu()
+    // if (tabuleiro[2] === jogador2 && tabuleiro[4] === jogador2 && tabuleiro[6] === jogador2) return jogadorOVenceu()
+    // if (tabuleiro[0] === jogador2 && tabuleiro[3] === jogador2 && tabuleiro[6] === jogador2) return jogadorOVenceu()
+    // if (tabuleiro[1] === jogador2 && tabuleiro[4] === jogador2 && tabuleiro[7] === jogador2) return jogadorOVenceu()
+    // if (tabuleiro[2] === jogador2 && tabuleiro[5] === jogador2 && tabuleiro[8] === jogador2) return jogadorOVenceu()
 
     // let casasMarcadas = 0
     // for (let i = 0; i < tabuleiro.length; i++) {
@@ -73,19 +89,54 @@ function rand(jogadores) {
     return jogadores[(Math.random() * jogadores.length) | 0]
 }
 
+let pontuacaoJogadorX = 0
+let pontuacaoJogadorO = 0
+
+const funcoes = {
+    jogadorXVenceu: function () {
+        pontuacaoJogadorX++
+        console.log('Jogador X venceu!!!', pontuacaoJogadorX)
+    },
+    jogadorOVenceu: function () {
+        pontuacaoJogadorO++
+        console.log('Jogador O venceu!!!', pontuacaoJogadorO)
+    }
+
+}
 
 
+// function jogadorXVenceu() {
+//     pontuacaoJogadorX++
+//     console.log('Jogador X venceu!!!', pontuacaoJogadorX)
+// }
 
+// function jogadorOVenceu() {
+//     pontuacaoJogadorO++
+//     console.log('Jogador O venceu!!!', pontuacaoJogadorO)
+// }
 
+function jogadorVencedor(jogador) {
+    funcoes['jogador' + jogador + 'Venceu']()
+}
 
 
 
 jogar(0)
-jogar(1)
-jogar(5)
 jogar(4)
-jogar(2)
-jogar(3)
+jogar(1)
 jogar(7)
-jogar(8)
-jogar(6)
+jogar(2)
+
+
+
+
+
+// jogar(0)
+// jogar(1)
+// jogar(5)
+// jogar(4)
+// jogar(2)
+// jogar(3)
+// jogar(7)
+// jogar(8)
+// jogar(6)
