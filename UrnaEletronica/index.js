494159bfb29dce964aa6d3480b13db2ca2b1701e
Leandro Cunha <leandrolespaul01@hotmail.com>
2022-08-20 15:24:23 -0300

    Urna Eletrônica

--- UrnaEletronica/index.js
+++ UrnaEletronica/index.js
@@ -4,6 +4,7 @@ const candidatos = [{
         numero: 13,
         numeroVotos: 0
 
+
     },
     {
         nome: 'Bolsonaro',
@@ -24,26 +25,61 @@ let votosBrancos = 0
 
 function confirmar(numeroDoCandidato) {
 
+
     const candidato = candidatos.find(candidato => candidato.numero === numeroDoCandidato)
-    console.log(candidato)
+
     if (!candidato) return votosNulos++
     candidato.numeroVotos++
     totalGeralVotos++
     console.log(candidato.nome, candidato.numero, candidato.partido, candidato.numeroVotos)
+    console.log(numeroVotos)
     console.log('Votos Nulos', votosNulos)
     console.log('Votos brancos', votosBrancos)
+
 
 }
 
 function branco() {
     votosBrancos++
+    candidatoDiv.innerHTML = ''
+    numeroDiv.innerHTML = ''
+    partidoDiv.innerHTML = ''
+    alert('Seu voto foi em branco.')
 }
 
-let displayNumeros = '' // variavel inicializada com uma string vazia
-const elements = document.getElementsByClassName('btnNum') // botões do HTML pegando pela CLASSE CSS
-for (let el of elements) { // Dando um for para passar por cada button
-    el.addEventListener('click', e => { // pegando o evento de click em cada botao
-        if (displayNumeros.length === 2) return // Se display (o click) for igual a tamanho 2
-        displayNumeros += e.target.value // concatena os clicks 
-    })
+function corrigir() {
+    candidatoDiv.innerHTML = ''
+    numeroDiv.innerHTML = ''
+    partidoDiv.innerHTML = ''
+    displayNumeros = ''
 }
+
+
+let candidatoDiv = document.getElementById('candidato')
+let numeroDiv = document.getElementById('numero')
+let partidoDiv = document.getElementById('partido')
+let displayNumeros = '' // variavel inicializada com uma string vazia
+
+function clique() {
+    const elements = document.getElementsByClassName('btnNum') // botões do HTML pegando pela CLASSE (CSS)
+    for (let el of elements) { // fazendo um for para passar por cada button
+        el.addEventListener('click', e => { // pegando o evento de click em cada botao
+            if (displayNumeros.length === 2) return // Se display (o click) for igual a tamanho 2
+            displayNumeros += e.target.value // concatena os clicks 
+
+            candidatoDiv.innerHTML = displayNumeros
+            candidatos.forEach(candidato => {
+                if (candidato.numero === parseInt(displayNumeros)) {
+                    candidatoDiv.innerHTML = `Candidato à Presidência: ${candidato.nome}`
+                    numeroDiv.innerHTML = `Numero do candidato: ${candidato.numero}`
+                    partidoDiv.innerHTML = `Partido do candidato: ${candidato.partido}`
+                }
+                // if (candidato.numero === 22) candidato.numeroVotos++
+                // if (candidato.numero === 13) candidato.numeroVotos++
+                // if (candidato.numero === 45) candidato.numeroVotos++
+            });
+
+        })
+    }
+}
+clique()
