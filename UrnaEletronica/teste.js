a4098f570abdac151624d8ce88691f5a37cb8d28
Leandro Cunha <leandrolespaul01@hotmail.com>
2022-09-15 00:23:35 -0300

    atua

--- UrnaEletronica/teste.js
+++ UrnaEletronica/teste.js
@@ -1,16 +1,43 @@
-let contador = 0
+// function contadoraDePalavras(frase) {
+//     frase = frase.toLowerCase()
+//     const objeto = {}
 
-function contadoraDePalavras(texto) {
-    const textoSVirgulaSponto = texto.replace(/,/g, ' ').replace('.', '')
+//     const textoSVirgulaSPonto = frase.replace(/,/g, ' ').replace('.', '').replace(': ', ' ')
 
-    const textoSeparado = textoSVirgulaSponto.split(' ')
+//     const fraseSeparada = textoSVirgulaSPonto.split(' ')
 
-    for (let i = 0; i < textoSeparado.length; i++) {
-        const element = textoSeparado[i];
-        console.log(element)
 
-    }
+//     for (let i = 0; i < fraseSeparada.length; i++) {
+//         if (!objeto[fraseSeparada[i]]) { // se o valor (0, 1, 2...) for undenfiend (falso)
+//             objeto[fraseSeparada[i]] = 1 // o valor passará a ser 1 
+//         } else {
+//             objeto[fraseSeparada[i]] = objeto[fraseSeparada[i]] + 1 // se o valor ja for 1 e vier outro valor soma + 1
+//         }
+//     }
 
+//     return objeto
+// }
+// console.log(contadoraDePalavras('mudei um texto aqui para mostrar que o texto pode ser mudado aqui.'))
+
+
+// function objeto() {
+//     var endereco = {
+//         rua: "Av do CPA",
+//         numero: 1293,
+//         bairro: "Jardim Cuiabá",
+//         cidade: "Cuiabá",
+//         uf: "MT"
+//     };
+//     console.log(`O usuário mora no ${endereco['cidade']}/${endereco['uf']} no bairro ${endereco['bairro']} cuiabá na ${endereco['rua']} numero ${endereco.numero}.`)
+// }
+
+// objeto()
+
+const arrayObj = {
+    nome: 'Leandro'
 }
+const nomeNovo = arrayObj.nome
+arrayObj['nome'] = 'leo'
+arrayObj['nome'] = nomeNovo
 
-contadoraDePalavras('Um texto aqui para ser lido, Um texto aqui para ser lido.')
+console.log(arrayObj)
